c1c16b4002f75da1d6264e2eab14e3398b4987f8
AdilsonManueTN <adilsondeoliveiramanuel@gmail.com>
2022-04-17 10:22:20 +0100

    contabilidade

---
+++ database/migrations/2022_04_17_091553_create_salarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salarios', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('categoria_id');
+            $table->decimal('salario_bruto',$precision = 8, $scale = 2);
+            $table->decimal('salario_liquido',$precision = 8, $scale = 2);
+            $table->decimal('salario_base',$precision = 8, $scale = 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salarios');
+    }
+}
